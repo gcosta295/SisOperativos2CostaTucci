895e3ac2b229537b18f38da38f0e443f91d0e47f
gcosta295 <156736652+gcosta295@users.noreply.github.com>
2026-03-03 19:33:12 -0400

    Commit inicial del repositorio

---
+++ src/main/java/com/mycompany/sisoperativos2costatucci/SisOperativos2CostaTucci.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sisoperativos2costatucci;
+
+/**
+ *
+ * @author gabri
+ */
+public class SisOperativos2CostaTucci {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
