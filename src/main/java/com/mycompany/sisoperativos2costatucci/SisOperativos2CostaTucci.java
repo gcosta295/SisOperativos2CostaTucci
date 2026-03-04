f53f74817fe2001aaf785e1e3598c03d1698e200
gcosta295 <156736652+gcosta295@users.noreply.github.com>
2026-03-03 20:46:30 -0400

    Update SisOperativos2CostaTucci.java

--- src/main/java/com/mycompany/sisoperativos2costatucci/SisOperativos2CostaTucci.java
+++ src/main/java/com/mycompany/sisoperativos2costatucci/SisOperativos2CostaTucci.java
@@ -11,6 +11,6 @@ package com.mycompany.sisoperativos2costatucci;
 public class SisOperativos2CostaTucci {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Woralo??ld!");
     }
 }
